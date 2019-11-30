1e4462ae911f7ab8221876e5a0e75d0954858d40
andriydovbenko <andriydovbenko1@gmail.com>
2019-11-30 16:10:39 +0100

    added method-solution task2

--- src/com/cursor/lesson2/SumOfThePositiveNumbers.java
+++ src/com/cursor/lesson2/SumOfThePositiveNumbers.java
@@ -9,5 +9,13 @@ package com.cursor.lesson2;
  */
 public class SumOfThePositiveNumbers {
 
-
+    public static int computingPositiveNumbers(int[] array) {
+        int sum = 0;
+        for (int i : array) {
+            if (i > 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
